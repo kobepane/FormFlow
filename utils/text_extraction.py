ed344989b26a2cf148a0055956cb1fc52e659fb7
coleherman43 <colesten@gmail.com>
2024-10-19 15:15:17 -0700

    Added PIL and tesseract requirements

--- utils/text_extraction.py
+++ utils/text_extraction.py
@@ -1,10 +1,14 @@
-# receive PIL 
-def text_extract() -> list[str]:
+from PIL import Image
+
+
+
+def text_extract(FILE: Image.Image) -> list[str]:
+    
     return ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 
 # this is what runs when the text extraction is needed
-def run_text_extract(FILE) -> list[str]:
+def run_text_extract(FILE: Image.Image) -> list[str]:
     if FILE:
         res: list[str] = text_extract(FILE)
     else:
