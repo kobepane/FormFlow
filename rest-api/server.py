01fb5b7865106d111af0d242a5f970ea476d7718
Kobe Pane <kobepane@gmail.com>
2024-10-19 17:45:55 -0700

    Receiving files at rest-api

--- rest-api/server.py
+++ rest-api/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 import csv
 
 app = Flask(__name__)
@@ -12,7 +12,8 @@ def handle_files():
         file = uploaded_files[key]
         file_names.append(file.filename)
         # Add your file processing logic here
-        
+        print(file)
+
     response = {
         "message": f"Successfully received {len(file_names)} files",
         "files": file_names
