cb79443f133287ed9cd7c58041928a8f8e459186
coleherman43 <colesten@gmail.com>
2024-10-19 20:45:54 -0700

    New config for start phrase

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 CSV_HEADERS: list[str] = ["First Name", "Last Name", "Phone Number", "Major(s)", "Email", "Year", "Date Signed", "Organizer Name"]
+START_PHRASE: str = "UO PALESTINE COALITION SUPPORT PETITION"
--- utils/run_utils.py
+++ utils/run_utils.py
@@ -3,7 +3,7 @@ from text_extraction import run_text_extract
 from clean_data import *
 
 # run takes a PDF file from the API section. It returns a list of information in order to the API section.
-def run(FILE):
+def run(FILE) -> list[str]:
     PIL_FILE = run_preprocess(FILE)
     data = run_text_extract(PIL_FILE)
     # res = clean_data(data)
