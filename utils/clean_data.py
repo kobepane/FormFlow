ec6bfa88444cbb7314a2f37ff536f9bf9e4a7ff7
coleherman43 <colesten@gmail.com>
2024-10-19 21:27:47 -0700

    Fix text_extraction

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -2,9 +2,9 @@ import config
 
 # get rid of anything before the start phrase (UO PALESTINE COALITION SUPPORT PETITION)
 def cut_first_chars(data: list[str]) -> list[str]:
-    
+    return [0,0]
 
 
 
 def run_clean(data: list[str]) -> list[str]:
-    return NotImplementedError
+    return NotImplementedError()
