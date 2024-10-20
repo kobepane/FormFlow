dcca079156f70cfde01ca74864c8f26d41543364
coleherman43 <colesten@gmail.com>
2024-10-19 20:22:54 -0700

    Made clean_data

--- utils/run_utils.py
+++ utils/run_utils.py
@@ -1,8 +1,10 @@
 from utils import preprocessing
 from text_extraction import run_text_extract
+from clean_data import *
 
 # run takes a PDF file from the API section. It returns a list of information in order to the API section.
 def run(FILE):
     PIL_FILE = do_prepocessing(FILE)
-    res = run_text_extract(PIL_FILE)
+    data = run_text_extract(PIL_FILE)
+    res = clean_data(data)
     return res
