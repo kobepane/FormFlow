cc01d8a6df9f6b5b0a21caa045537336588c8ef5
coleherman43 <colesten@gmail.com>
2024-10-20 03:14:00 -0700

    new config

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 CSV_HEADERS: list[str] = ["First Name", "Last Name", "Phone Number", "Major(s)", "Email", "Year", "Date Signed", "Organizer Name"]
-START_PHRASE: list[str] = ["UO", "PALESTINE", "COALITION", "SUPPORT", "PETITION"]
+START_KEYWORD: str = "earnest."
